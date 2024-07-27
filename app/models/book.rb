9e7bc8385c86602af72aa82a8055f70341d5c17f
kangaloos <166692206+kangaloos@users.noreply.github.com>
2024-07-27 11:30:12 +0000

    課題：Bookers2を作成しよう

---
+++ app/models/book.rb
@@ -0,0 +1,13 @@
+class Book < ApplicationRecord
+   has_one_attached :image
+   validates:title, presence: true
+   validates:body, presence: true, length: { maximum: 200 }
+    
+   
+   
+  belongs_to :user
+  def introduction
+    # 紹介文を返す処理を書く
+  end
+
+end
