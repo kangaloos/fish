f3f184288c30e694814198757d160703f60ca31d
kangaloos <166692206+kangaloos@users.noreply.github.com>
2024-09-22 12:40:37 +0000

    Admin

--- Gemfile
+++ Gemfile
@@ -78,7 +78,11 @@ end
 gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
+gem 'rails_admin'
+gem 'cancancan'
+gem 'rails-i18n'
 
 group :development do
   gem "rails-erd"
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_22_061745) do
+ActiveRecord::Schema.define(version: 2024_09_22_120816) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -52,7 +52,7 @@ ActiveRecord::Schema.define(version: 2024_09_22_061745) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
-    t.integer "post_image_id"
+    t.integer "point_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2024_09_22_061745) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.text "introduction"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
